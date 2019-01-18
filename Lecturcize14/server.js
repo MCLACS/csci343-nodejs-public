aa1adbf5e54b4d619a1bd0a599672fd55ef7c4fa
Mark Cohen <mark.cohen@mcla.edu>
2019-01-18 20:45:26 +0000

    added lecturcize 14

---
+++ Lecturcize14/server.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const app = express();
+
+var mysql = require('mysql');
+
+// install first using npm install bcrypt
+const bcrypt = require('bcrypt');
+
+const conInfo = 
+{
+    host: process.env.IP,
+    user: process.env.C9_USER,
+    password: "",
+    database: "USERTEST"
+};
+
+var session = require('express-session'); 
+app.use(session({ secret: 'happy jungle', 
+                  resave: false, 
+                  saveUninitialized: false, 
+                  cookie: { maxAge: 60000 }}))
+
+app.all('/', whoIsLoggedIn);                  
+app.all('/register', register);
+app.all('/login', login);
+app.all('/logout', logout);                  
+
+app.listen(process.env.PORT,  process.env.IP, startHandler())
+
+function startHandler()
+{
+  console.log('Server listening on port ' + process.env.PORT)
+}
+
+function whoIsLoggedIn(req, res)
+{
+  if (req.session.user == undefined)
+    writeResult(req, res, {'result' : 'Nobody is logged in.'});
+  else
+    writeResult(req, res, req.session.user);
+}
+
+function register(req, res)
+{
+  if (req.query.email == undefined || !validateEmail(req.query.email))
+  {
+    writeResult(req, res, {'error' : "Please specify a valid email"});
+    return;
+  }
+
+  if (req.query.password == undefined || !validatePassword(req.query.password))
+  {
+    writeResult(req, res, {'error' : "Password must have a minimum of eight characters, at least one letter and one number"});
+    return;
+  }
+
+  var con = mysql.createConnection(conInfo);
+  con.connect(function(err) 
+  {
+    if (err) 
+      writeResult(req, res, {'error' : err});
+    else
+    {
+      let hash = bcrypt.hashSync(req.query.password, 10);
+      con.query("INSERT INTO USER (USER_EMAIL, USER_PASS) VALUES (?, ?)", [req.query.email, hash], function (err, result, fields) 
+      {
+        if (err) 
+        {
+          if (err.code == "ER_DUP_ENTRY")
+            err = "User account already exists.";
+          writeResult(req, res, {'error' : err});
+        }
+        else
+        {
+          con.query("SELECT * FROM USER WHERE USER_EMAIL = ?", [req.query.email], function (err, result, fields) 
+          {
+            if (err) 
+              writeResult(req, res, {'error' : err});
+            else
+            {
+              req.session.user = {'result' : {'id': result[0].USER_ID, 'email': result[0].USER_EMAIL}};
+              writeResult(req, res, req.session.user);
+            }
+          });
+        }
+      });
+    }
+  });
+  
+}
+
+function login(req, res)
+{
+  if (req.query.email == undefined)
+  {
+    writeResult(req, res, {'error' : "Email is required"});
+    return;
+  }
+
+  if (req.query.password == undefined)
+  {
+    writeResult(req, res, {'error' : "Password is required"});
+    return;
+  }
+  
+  var con = mysql.createConnection(conInfo);
+  con.connect(function(err) 
+  {
+    if (err) 
+      writeResult(req, res, {'error' : err});
+    else
+    {
+      con.query("SELECT * FROM USER WHERE USER_EMAIL = ?", [req.query.email], function (err, result, fields) 
+      {
+        if (err) 
+          writeResult(req, res, {'error' : err});
+        else
+        {
+          if(result.length == 1 && bcrypt.compareSync(req.query.password, result[0].USER_PASS))
+          {
+            req.session.user = {'result' : {'id': result[0].USER_ID, 'email': result[0].USER_EMAIL}};
+            whoIsLoggedIn(req, res);
+          }
+          else 
+          {
+            writeResult(req, res, {'error': "Invalid email/password"});
+          }
+        }
+      });
+    }
+  });
+}
+
+function logout(req, res)
+{
+  req.session.user = undefined;
+  writeResult(req, res, {'result' : 'Nobody is logged in.'});
+}
+
+function writeResult(req, res, obj)
+{
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify(obj));
+  res.end('');
+}
+
+function validateEmail(email) 
+{
+  if (email == undefined)
+  {
+    return false;
+  }
+  else
+  {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+}
+
+function validatePassword(pass)
+{
+  if (pass == undefined)
+  {
+    return false;
+  }
+  else
+  {
+    var re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return re.test(pass);
+  }
+}
